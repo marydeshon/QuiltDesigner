9b9db064b6481437659f68710c76c8214062cc42
marydeshon <marydeshon@gmail.com>
2026-06-12 10:06:53 -0400

    Work on flag pattern

--- QuiltDesigner/Pages/Flag.cshtml.cs
+++ QuiltDesigner/Pages/Flag.cshtml.cs
@@ -13,12 +13,12 @@ public class Flag(
     public List<Shape> Shapes { get; set; } = new List<Shape>();
     public List<string> Swatchfilenames { get; set; } = new List<string>();
     public List<Swatch> Swatches { get; set; } = new List<Swatch>();
-    public string Swatchfilepath { get; set; } = "/images/swatches2/";
+    public string Swatchfilepath { get; set; } = "/images/flag_swatches/";
 
     public void OnGet()
     {
         Shapes = patternService.GetAll();
-        string tmpPath = Path.Combine(environment.WebRootPath, "images", "swatches");
+        string tmpPath = Path.Combine(environment.WebRootPath, "images", "flag_swatches");
         Swatches = swatchService.GetAll(tmpPath, Swatchfilenames);
     }
 }
